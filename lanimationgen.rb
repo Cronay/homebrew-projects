c415657c39736665f2bdf1056d8f4ad0a465acb7
Yannic Borgfeld <inf@yannic-borgfeld.de>
2020-02-23 15:25:16 +0100

    Update formula

--- lanimationgen.rb
+++ lanimationgen.rb
@@ -3,6 +3,7 @@ class Lanimationgen < Formula
   homepage "https://github.com/Cronay/LAnimationGen"
   head "https://github.com/Cronay/LAnimationGen.git"
   url "https://github.com/Cronay/LAnimationGen/archive/v0.1.tar.gz"
+  sha256 "f7762dea0c0691c044d04d18b0cd2abfc079ce0d1cf777d67287a760f31fe5e5"
 
   depends_on :xcode => ["11", :build]
 
