2bfbd081536b86d055f2516c9171c9d010242884
Yannic <yannic.borgfeld@dmhub.de>
2020-02-24 10:11:22 +0100

    update formula

--- lanimationgen.rb
+++ lanimationgen.rb
@@ -1,7 +1,7 @@
 class Lanimationgen < Formula
   desc "Generates Swift code for Lottie Animations"
   homepage "https://github.com/Cronay/LAnimationGen"
-  url "https://github.com/Cronay/LAnimationGen.git", :tag => "v0.1"
+  url "https://github.com/Cronay/LAnimationGen.git"
   head "https://github.com/Cronay/LAnimationGen.git"
   depends_on :xcode => ["11", :build]
 
