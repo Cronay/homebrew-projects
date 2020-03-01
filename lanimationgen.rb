6b0c50cfe25911334c177badae00f60b7ee6e6a4
Yannic Borgfeld <inf@yannic-borgfeld.de>
2020-03-01 20:51:55 +0100

    Update formula

--- lanimationgen.rb
+++ lanimationgen.rb
@@ -2,7 +2,7 @@ class Lanimationgen < Formula
   desc "Generates a swift class with static values for Lottie animations"
   homepage "https://github.com/Cronay/LAnimationGen"
   url "https://github.com/Cronay/LAnimationGen/archive/v0.0.1.tar.gz"
-  sha256 "51867210e9032bf99daed24910b70710d49dbac98781525701e83bd9bf94dbbb"
+  sha256 "fa635f182dcdde801f5d3bbb61b66eb02ab01e2d5f8bdd1a3053420dc779cfb5"
 
   def install
     system "make", "install", "prefix=#{prefix}"
