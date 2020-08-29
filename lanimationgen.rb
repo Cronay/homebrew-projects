600b9ed01f909c61f90734bea43229eda956af57
Cronay <inf@yannic-borgfeld.de>
2020-08-29 11:57:20 +0200

    Update for new release of LAnimationGen

--- lanimationgen.rb
+++ lanimationgen.rb
@@ -1,8 +1,8 @@
 class Lanimationgen < Formula
   desc "Generates a swift class with static values for Lottie animations"
   homepage "https://github.com/Cronay/LAnimationGen"
-  url "https://github.com/Cronay/LAnimationGen/archive/v0.0.1.tar.gz"
-  sha256 "fa635f182dcdde801f5d3bbb61b66eb02ab01e2d5f8bdd1a3053420dc779cfb5"
+  url "https://github.com/Cronay/LAnimationGen/archive/v0.0.2.tar.gz"
+  sha256 "d19040186bf83d8bf532f716e0836862d3572bf96e527100fd817143f5c3c1c6"
 
   def install
     system "make", "install", "prefix=#{prefix}"
